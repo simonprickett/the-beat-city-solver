e22fceb12eadd3ed579da0de07c7122758cdf393
Simon Prickett <simon@crudworks.org>
2025-03-02 15:52:39 +0000

    Work in progress.

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 Solves "which UK city or cities contains no letters from the phrase..." problems, often posed on Dean Jackson's The Beat and other BBC Radio Nottingham shows.
 
 I am using the UK Government's [list of cities](https://www.gov.uk/government/publications/list-of-cities/list-of-cities-html).
+
+## Running Locally
+
+Use any web server to run this locally.  For example, the one built into Python 3:
+
+```bash
+python -m http.server
+```
+
+This will serve the project on port 8000: `https://localhost:8000`
+
+## Examples
+
+festival -> York
+christmas -> Ely, London, Dundee
---
+++ solver.js
@@ -0,0 +1,38 @@
+const response = await fetch('cities.json');
+const cities = (await response.json()).cities;
+
+const phraseInput = document.getElementById('phraseInput');
+
+phraseInput.addEventListener('keyup', async function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    const phrase = phraseInput.value.trim().toLowerCase();
+
+    if (phrase.length === 0) {
+      // TODO handle this better in the UI.
+      alert('enter something');
+      return;
+    }
+
+    // Get uniques from the phrase.
+    const phraseUniques = new Set(phraseInput.value.trim().replaceAll(' ', '').toLowerCase());
+
+    // Find cities that have no chars from phraseUniqueChars.
+    const matchingCities = cities.filter((city) => {
+      const cityUniques = new Set(city.replaceAll(' ', '').toLowerCase());
+      const commonChars = cityUniques.intersection(phraseUniques);
+
+      return commonChars.size === 0;
+    });
+  
+    // TODO output matches in a nicer way.
+    console.log(matchingCities);
+
+    // TODO output the phrase too
+
+    // TODO clear the input
+  }
+});
+
+phraseInput.focus();
+
